b7f4ec151a652b784ae1f4a43f53ee5133897ed1
fishfive <321286823@qq.com>
2022-02-04 00:19:49 +0800

    BGWiFiConfig V1.0.1

--- examples/zdyhtml_demo/zdyhtml_demo.ino
+++ examples/zdyhtml_demo/zdyhtml_demo.ino
@@ -5,6 +5,7 @@
     开发文档：https://www.cbug.top/1app/bgwificonfig
     Gitee地址：https://gitee.com/fish_five/BGWiFiConfig
     GitHub地址：https://github.com/fish-five/BGWiFiConfig
+    微信公众号：蚍蜉荣光  官方网站：www.cbug.top
  **/
  
 #include <BGWiFiConfig.h>
