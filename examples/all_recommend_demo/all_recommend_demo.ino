abbfe8260eb5115d7e1862426c83186383b9c81d
fish-five <321286823@qq.com>
2024-11-27 23:27:57 +0800

    BGWiFiConfig v1.1.0

--- examples/all_recommend_demo/all_recommend_demo.ino
+++ examples/all_recommend_demo/all_recommend_demo.ino
@@ -1,8 +1,8 @@
 /**
     BGWiFiConfig示例代码
     开发板： ESP32
-    实现功能：新增兼容模式，优化解决部分手机配网WiFi自动断连问题，
-    启用兼容模式，配网IP将从192.168.22.22变为192.168.22.1。
+    实现功能：提供一个推荐使用的demo，以契合BGWiFiConfig诸多新增库函数，
+    本demo建议配合微信小程序配网（小程序名：配网库）使用。
     开发文档：https://www.cbug.top/1app/bgwificonfig
     Gitee地址：https://gitee.com/fish_five/BGWiFiConfig
     GitHub地址：https://github.com/fish-five/BGWiFiConfig
@@ -14,13 +14,9 @@ BGWiFiConfig wifipw;
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  wifipw.setConFailReset(3, 10,true);
+  wifipw.setConFailReset(2, 10,true);
   wifipw.useSpaceWiFi(true);
   wifipw.autoStart(true);
-  /**setCompatibleMode
-  1.xCMTag为true：启用兼容模式，配网IP为：192.168.22.1
-  2.xCMTag为false：关闭兼容模式，配网IP为：192.168.22.22
-  ***/
   wifipw.setCompatibleMode(true);      
   wifipw.begin();
 }
